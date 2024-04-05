a053435ac0978d11e84692a699eb081d6e4a6421
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-04-04 19:34:33 -0700

    final commit

--- public/index.js
+++ public/index.js
@@ -1,15 +1,33 @@
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
+
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
-    // let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX,TRP:TSX&interval=1min&apikey=3366ede775a0435aad985b6252682f46')
-    // let result = await response.json()
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX,TRP:TSX&interval=1min&apikey=3366ede775a0435aad985b6252682f46')
+    let result = await response.json()
 
-    // const {GME, MSFT, DIS, BTNX} = result 
 
-    const { GME, MSFT, DIS, BNTX } = mockData;
+    let { GME, MSFT, DIS, BNTX } = result;
+
+    // const { GME, MSFT, DIS, BNTX } = mockData;
 
     const stocks = [GME, MSFT, DIS, BNTX];
 
@@ -30,26 +48,37 @@ async function main() {
         });
 
 
-    
-    function getColor(stock){
-        if(stock === "GME"){
-            return 'rgba(61, 161, 61, 0.7)'
+     // High Chart
+     new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    findHighest(stock.values)
+                ))
+            }]
         }
-        if(stock === "MSFT"){
-            return 'rgba(209, 4, 25, 0.7)'
-        }
-        if(stock === "DIS"){
-            return 'rgba(18, 4, 209, 0.7)'
-        }
-        if(stock === "BNTX"){
-            return 'rgba(166, 43, 158, 0.7)'
-        }
-    }
-    
-    
-    
+    });
 
 
+}
+
+function findHighest(values) {
+    let highest = 0;
+    values.forEach(value => {
+        if (parseFloat(value.high) > highest) {
+            highest = value.high
+        }
+    })
+    return highest
 }
 
 
