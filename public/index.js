5d75c501e866f7fdd069dc8d43b8e514f97c8f31
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-04-04 19:12:08 -0700

    first chart complete

--- public/index.js
+++ public/index.js
@@ -4,6 +4,54 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX,TRP:TSX&interval=1min&apikey=3366ede775a0435aad985b6252682f46')
+    // let result = await response.json()
+
+    // const {GME, MSFT, DIS, BTNX} = result 
+
+    const { GME, MSFT, DIS, BNTX } = mockData;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    stocks.forEach( stock => stock.values.reverse())
+
+// Time Chart
+        new Chart(timeChartCanvas.getContext('2d'), {
+            type: 'line',
+            data: {
+                labels: stocks[0].values.map(value => value.datetime),
+                datasets: stocks.map( stock => ({
+                    label: stock.meta.symbol,
+                    data: stock.values.map(value => parseFloat(value.high)),
+                    backgroundColor:  getColor(stock.meta.symbol),
+                    borderColor: getColor(stock.meta.symbol),
+                }))
+         }
+        });
+
+
+    
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+    
+    
+    
+
+
 }
+
+
 
 main()
